6f7dcd7b4ed2c35925fd4049af4eab1146a0bb08
salma-adl <faizirsali@gmail.com>
2026-01-15 14:33:44 +0700

    Allow therapist role to access Filament panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,6 +57,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->is_superuser || $this->hasRole('admin');
+        return $this->is_superuser || $this->hasAnyRole(['admin', 'therapist']);
     }
 }
